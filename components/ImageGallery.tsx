5e0e5628090c6d1f1bbc6e654ec4a7bf17704a31
gabrielfelipedy <gabriel.felipe.dy@gmail.com>
2025-04-23 21:14:25 -0300

    prepare a little more image gallery

--- components/ImageGallery.tsx
+++ components/ImageGallery.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+
+import model_img from "/public/assets/img/model.jpg";
 
 const ImageGallery = () => {
   return (
-    <div>ImageGallery</div>
-  )
-}
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-20">
+      <Image src={model_img} alt="model_img" className="rounded-[0.75rem]" />
 
-export default ImageGallery
+      <Image src={model_img} alt="model_img" className="rounded-[0.75rem]" />
+
+      <Image src={model_img} alt="model_img" className="rounded-[0.75rem]" />
+
+      <Image src={model_img} alt="model_img" className="rounded-[0.75rem]" />
+    </div>
+  );
+};
+
+export default ImageGallery;
