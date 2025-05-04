960066d5be198d131484401e98db0e628b82273a
gabrielfelipedy <gabriel.felipe.dy@gmail.com>
2025-05-04 17:35:06 -0300

    make initial animation better

--- components/Presentation.tsx
+++ components/Presentation.tsx
@@ -12,56 +12,17 @@ if (typeof window !== "undefined") {
 
 const Presentation = () => {
   const container = useRef<HTMLElement | any>();
-  const name = useRef<HTMLElement | any>();
-  const rolling = useRef<HTMLElement | any>();
-  const description = useRef<HTMLElement | any>();
 
   useGSAP(
     () => {
-      const name_split = new SplitText(name.current, {
-        type: "lines",
-        linesClass: "line",
-      });
-
-      gsap.from(name_split.lines, {
+      gsap.from(".element", {
         y: 10,
         opacity: 0,
-        stagger: 0.1,
-        duration: 0.5,
+        stagger: 0.4,
+        duration: 0.8,
         ease: "power3.out",
+        delay: 0.5
       });
-
-      const rolling_split = new SplitText(rolling.current, {
-        type: "lines",
-        linesClass: "line",
-      });
-
-      gsap.from(rolling_split.lines, {
-        y: 10,
-        opacity: 0,
-        stagger: 0.1,
-        duration: 0.5,
-        ease: "power3.out",
-      });
-
-      const description_split = new SplitText(description.current, {
-        type: "lines",
-        linesClass: "line",
-      });
-
-      gsap.from(description_split.lines, {
-        y: 10,
-        opacity: 0,
-        stagger: 0.1,
-        duration: 0.5,
-        ease: "power3.out",
-      });
-
-      return () => {
-        name_split.revert();
-        rolling_split.revert();
-        description_split.revert();
-      };
     },
     { scope: container }
   );
@@ -71,26 +32,17 @@ const Presentation = () => {
       className="flex items-center justify-center min-h-[95vh] relative overflow-hidden max-container"
       ref={container}
     >
-      <div
-        className="flex flex-col items-centertext-center p-5 rounded-[0.75rem] max-w-[800px]"
-        ref={name}
-      >
-        <p className="dark:text-light-800 font-small tracking-widest my-name">
+      <div className="flex flex-col items-centertext-center p-5 rounded-[0.75rem] max-w-[800px]">
+        <p className="dark:text-light-800 font-small tracking-widest element">
           GABRIEL FELIPE
         </p>
-        <div
-          className="font-title font-bold text-light-400 text-wrap my-rolling"
-          ref={rolling}
-        >
+        <div className="font-title font-bold text-light-400 text-wrap element">
           Desenvolvedor de Software e Designer Gráfico
         </div>
 
         <div className="mt-10"></div>
 
-        <div
-          className="font-section text-wrap description dark:text-light-800"
-          ref={description}
-        >
+        <div className="font-section text-wrap element dark:text-light-800">
           Olá, muito prazer! Sou Gabriel Felipe, sou Desenvolvedor de Software e
           Designer gráfico. Utilizo meus conhecimentos e experiência para
           oferecer soluções personalizadas para você.
